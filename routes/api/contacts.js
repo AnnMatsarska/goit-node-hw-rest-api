b741dbdfc0123765d19fa43475bd9e77655f4bc6
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-12-07 14:46:37 +0200

    add HttpError and joi validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const contacts = require("../../models/contacts");
 const router = express.Router();
-// const Joi = require("joi");
+const { HttpError } = require("../../helpers");
+const Joi = require("joi");
+
+const addScheme = Joi.object({
+  name: Joi.string()
+    .min(2)
+    .max(30)
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+  email: Joi.string()
+    .min(7)
+    .max(30)
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+  phone: Joi.string()
+    .min(7)
+    .max(30)
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -16,15 +35,18 @@ router.get("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const data = await contacts.getContactById(contactId);
+    if (!data) {
+      throw HttpError(404, "Not found");
+    }
     res.json(data);
   } catch (error) {
     next(error);
   }
 });
 
-// router.post("/", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
 // router.delete("/:contactId", async (req, res, next) => {
 //   res.json({ message: "template message" });
