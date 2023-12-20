0c992d411b64177867b4e95086a1512318850c54
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-12-20 23:15:17 +0200

    add register route

--- models/user.js
+++ models/user.js
@@ -7,7 +7,6 @@ const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 // ---JOI Schema --------------------------------
 
 const registerSchema = Joi.object({
-  name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
@@ -24,27 +23,30 @@ const schemas = {
 
 // ---Mongoose Schema --------------------------------
 
-const userSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, "Set password for user"],
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: String,
-});
+  { versionKey: false }
+);
 
 userSchema.post("save", handleMongooseError);
 
-const User = model("contact", userSchema);
+const User = model("user", userSchema);
 
 module.exports = {
   User,
