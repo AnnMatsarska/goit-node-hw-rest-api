9a3d3013c2b88bda0cdffa845e6f6c109b1a62cc
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-12-21 13:31:33 +0200

    add login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const { validateBody } = require("../../middlewares");
 const { schemas } = require("../../models/user");
-const ctrl = require("../../controllers/auth");
+const ctrl = require("../../controllers/user");
 
 // --signup
-
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+// --signin
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
