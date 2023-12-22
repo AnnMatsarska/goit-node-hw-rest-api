c9a68d1826c266c5ee0b49467cfc0f01ce2f7b7a
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-12-22 19:23:39 +0200

    add favorite params request

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,12 +3,18 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const data = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  });
+  const data =
+    favorite === undefined
+      ? await Contact.find({ owner }, "-createdAt -updatedAt", {
+          skip,
+          limit,
+        })
+      : await Contact.find({ owner, favorite }, "-createdAt -updatedAt", {
+          skip,
+          limit,
+        });
   res.json(data);
 };
 
