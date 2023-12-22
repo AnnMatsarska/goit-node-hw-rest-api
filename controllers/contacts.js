b7825e50556b9a426c86d3771b09b2b1daafd023
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-12-22 18:31:37 +0200

    add pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const data = await Contact.find({ owner });
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const data = await Contact.find({ owner }, "-createdAt iupdatedAt", {
+    skip,
+    limit,
+  });
   res.json(data);
 };
 
