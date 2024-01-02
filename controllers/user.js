0a2d6340811ff9b8c755908650bf27e5360ec2eb
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-01-02 19:36:55 +0200

    add: nodemailer sendEmail helper

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
+const { nanoid } = require("nanoid");
 
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
@@ -21,11 +22,13 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
   res.status(201).json({
     user: {
